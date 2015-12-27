0391f6c5e706a7f17655ff50322d49cd141b91d3
einnuJ <junnie.sim@stonybrook.edu>
2015-12-26 22:42:20 -0500

    PawnPiece has been fleshed out; the isValidMove() function is a little verbose and could use rework, but it should work. Testing would be nice.

--- src/game_logic/game_objects/PawnPiece.java
+++ src/game_logic/game_objects/PawnPiece.java
@@ -30,6 +30,26 @@ public class PawnPiece extends ChessPiece{
 
     @Override
     protected boolean isValidMove(int[] target) {
+        // Again, we assume the target is already within board bounds
+
+        int x = this.location[0];
+        int y = this.location[1];
+
+        if ((target[0] == x) && (target[1] == y + 1)) {
+            return true;
+        }
+
+        if ((target[0] == x) && (target[1] == y + 2) && (y == 1)) {
+            return true;
+        }
+
+        // The Capture Case
+        if (((target[0] == x + 1) || ((target[0] == x - 1) && x > 0)) &&
+                (target[1] == y
+                + 1)) {
+            return true;
+        }
+
         return false;
     }
 }
